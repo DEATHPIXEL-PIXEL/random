053e509fa03afc7a5bd0a6f4fc127c537ecebd22
DEATHPIXEL-PIXEL <zayaankhan337@gmail.com>
2024-12-11 19:46:32 +0530

    first commit

---
+++ functions.py
@@ -0,0 +1,6 @@
+import math
+print(math.ceil(3.15))
+
+print(math.floor(3.15))
+
+print(math.factorial(6))
---
+++ l.py
@@ -0,0 +1,9 @@
+import random
+
+randomnumber=random.randint(1,8)
+print(randomnumber)
+
+
+list=["zayaan", "bill", "shrey sir"]
+randomname=random.choice(list)
+print(randomname)
